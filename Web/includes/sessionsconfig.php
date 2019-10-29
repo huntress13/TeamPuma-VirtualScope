8c00e5407b77c0a09af3e3b01f40f5f20537e3ac
rsvagle <rsvagle@gmail.com>
2019-10-29 18:47:26 -0500

    cleanup

--- Web/includes/sessionsconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start(); //start the session
-    $username = isset($_SESSION['username']) ? $_SESSION['username'] : 'Guest';
-    $name = isset($_SESSION['name']) ? $_SESSION['name'] : 'Guest';
-    $loggedIn = isset($_SESSION['loggedIn']) ? $_SESSION['loggedIn'] : FALSE;
-    $userType = isset($_SESSION['userType']) ? $_SESSION['userType'] : 'none';
-?>
