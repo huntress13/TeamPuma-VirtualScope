b45f372d621436e9146b3e5abda70b10c5995886
huntress13 <48101419+huntress13@users.noreply.github.com>
2019-11-03 21:08:23 -0600

    classpassword delete button

--- Web/classpasswordpage.php
+++ Web/classpasswordpage.php
@@ -36,7 +36,7 @@
   <div class="card-body">
     <div class="container">
 
-<!-- Modify class password -->
+<!-- Add class password -->
  <div class="container" style="margin-top:1px">
       <h5>Add Class Password</h5>
         <form id="add-password" class="form-inline" action="includes/classpassword.inc.php" method="post">
@@ -50,53 +50,65 @@
           <input type="text" name = "class_password" placeholder="Class Password" class="form__input" />
         </div>
         <div class="form-group">
-          <button class="btn" name="classpassword-submit" type="submit" class="form__input" >Submit</button>
+          <button class="btn" name="addcpwd-submit" type="submit" class="form__input" >Submit</button>
         </div>
         </form>
 </div>
+<!--Delete class password-->
+<div class="card" style="margin-bottom:30px">
 
+        <div class="card-header">
+            Existing Class Passwords
+        </div>
 
-<!-- DISPLAY class password -->
- <div class="container" style="margin-top:20px">
-  <!-- Title -->
-<h5>Existing Passwords</h5>
-    <div class="container">
-      <div class="row">
-        <div class="table-responsive">
-          <?php include "includes/dbh.inc.php";
-          $sql = "SELECT course_name, section, class_password FROM class_passwords ORDER BY cpid DESC;";
-          $i = 0;
-          if($result = mysqli_query($conn, $sql)){
-            if(mysqli_num_rows($result) > 0){
-              echo "<table class=\"table table-striped text-center\">";
-                echo "<tr>";
-                 echo "<th class=\"text-center\">Course Name</th>";
-                 echo "<th class=\"text-center\">Section</th>";
-                 echo "<th class=\"text-center\">Class Password</th>";
-                echo "</tr>";
-              while($row = mysqli_fetch_array($result)){
-                echo "<tr>";
-                 echo "<td>" . $row['course_name'] . "</td>";
-                 echo "<td>" . $row['section'] . "</td>";
-                 echo "<td>" . $row['class_password'] . "</td>";
-                echo "</tr>";
-              }
-            echo "</table>";
-            mysqli_free_result($result);
-          } else{
-            echo "no class passwords exists with your query";
-          }
-
-         } else{
-          echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-          }
-          mysqli_close($conn);
-          ?>
-      </div>
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="table-responsive">
+                    <?php include "includes/dbh.inc.php";
+                        $sql = "SELECT * FROM class_passwords";
+                        $i = 0;
+                        if($result = mysqli_query($conn, $sql)){
+                            $numberOfRows = mysqli_num_rows($result);
+                            if(mysqli_num_rows($result) > 0){
+                                    echo "<table class=\"table table-striped text-center\">";
+                                        echo "<tr>";
+                                            echo "<th class=\"text-center\">ID</th>";
+                                            echo "<th class=\"text-center\">Course name</th>";
+                                            echo "<th class=\"text-center\">Section</th>";
+                                            echo "<th class=\"text-center\">Class Password</th>";;
+                                            echo "<th class=\"text-center\"></th>";
+                                        echo "</tr>";
+                                    while($row = mysqli_fetch_array($result)){
+                                        echo "<form id=\"classpassword-form". $i++ ."\" method=\"POST\" action=\"includes/deleteclasspassword.inc.php\" class=\"form-horizontal\">";
+                                            echo '<input type="hidden" id="classpasswordID" name="cpid" value="'. $row['cpid'] . '">';
+                                            echo "<tr>";
+                                                echo "<td>" . $row['cpid'] . "</td>";
+                                                echo "<td>" . $row['course_name'] . "</td>";
+                                                echo "<td>" . $row['section'] . "</td>";
+                                                echo "<td>" . $row['class_password'] . "</td>";
+                                                
+                                                
+                                                echo "<td><button type=\"submit\" class=\"btn\" name=\"deletecpwd-submit\">delete</button></td>";
+                                            echo "</tr>";
+                                        echo "</form>";
+                                    
+                                }
+                                echo "</table>";
+                                // Free result set
+                                mysqli_free_result($result);
+                            } else{
+                                echo "No records matching your query were found.";
+                            }
+                        } else{
+                            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-
 
 </div>
 </div>
--- Web/includes/deleteclasspassword.inc.php
+++ Web/includes/deleteclasspassword.inc.php
@@ -1,26 +1,19 @@
 <?php
-if (isset($_POST['addcpwd-submit'])) {
+if (isset($_POST['deletecpwd-submit'])) {
 
 	require 'dbh.inc.php';
 
 	//fetch info from admin
-	$course_name = $_POST['course_name'];
-	$section = $_POST['section'];
-	$class_password = $_POST['class_password'];
-
-	//confirm all fields are filled in, if not, exit
-	if (empty($course_name) || empty($section) || empty($class_password)){
-		header("Location: ../loginpage.php?error=emptyfield");
-		exit();
-	}else{
-		$sql = "INSERT INTO class_passwords(course_name, section, class_password) VALUES (?,?,?);";
+	$cpid = $_POST['cpid'];
+	
+		$sql = "DELETE FROM class_passwords WHERE cpid = ?";
 		$stmt = mysqli_stmt_init($conn);
 		if (!mysqli_stmt_prepare($stmt, $sql)) {
 				//checking to see if sql statement is valid
 				header("Location: ../loginpage.php?error=sqlstatementerror");
 				exit();
 			}else{
-				mysqli_stmt_bind_param($stmt, "sss", $course_name, $section, $class_password);
+				mysqli_stmt_bind_param($stmt, "d", $cpid);
 					mysqli_stmt_execute($stmt);
 
 
@@ -28,10 +21,10 @@ if (isset($_POST['addcpwd-submit'])) {
 					exit();
 			}
 
-	}
+	
 	msqli_stmt_close($stmt);
 	mysqli_close($conn);
-}
+	}
 
 
 
