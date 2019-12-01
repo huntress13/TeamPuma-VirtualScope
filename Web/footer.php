365abf003a9e0f7a93552edabf8b877c44d25a08
rsvagle <rsvagle@gmail.com>
2019-12-01 16:34:48 -0600

    removed contact from footer

--- Web/footer.php
+++ Web/footer.php
@@ -2,6 +2,5 @@
   <div class="container">
     <span class="text-muted">&copy 2019 Team Puma<br/>
       <a href="https://www.metrostate.edu">Metropolitan State University</a><br/>
-      <a href="#">Contact Us</a></span>
   </div>
 </footer>
